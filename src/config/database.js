dffa6ade75edc9359366b359d697163c812dbf49
Elian Muñoz <emunozsalas@icloud.com>
2026-02-18 06:39:15 -0300

    feat: quitar log de variables cargadas

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-console.log('Variables cargadas:', {
-  DB_NAME: process.env.DB_NAME,
-  DB_USER: process.env.DB_USER,
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-});
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
